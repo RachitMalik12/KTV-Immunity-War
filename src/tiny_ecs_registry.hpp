7c92ccaad74fbed0dfd9a6c9c11cc834616cea65
Bruce <yichen89@msn.com>
2021-10-23 20:46:01 -0700

    implemented a simple text based menu. you can select 1p/2p as well as 3 resolution mode.

--- src/tiny_ecs_registry.hpp
+++ src/tiny_ecs_registry.hpp
@@ -67,14 +67,12 @@ public:
 	}
 
 	void list_all_components() {
-		printf("Debug info on all registry entries:\n");
 		for (ContainerInterface* reg : registry_list)
 			if (reg->size() > 0)
 				printf("%4d components of type %s\n", (int)reg->size(), typeid(*reg).name());
 	}
 
 	void list_all_components_of(Entity e) {
-		printf("Debug info on components of entity %u:\n", (unsigned int)e);
 		for (ContainerInterface* reg : registry_list)
 			if (reg->has(e))
 				printf("type %s\n", typeid(*reg).name());
